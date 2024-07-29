28578572527eaf4e86c9e886feb9003cec11089a
ibelengu <isaac.belenguer-lliso@capgemini.com>
2024-07-29 14:26:53 +0200

    third commit

--- .history/src/main/java/com/example/demo/domain/model/price/PriceRepository_20240710110616.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.model.price;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface PriceRepository {
-    Price getPriceToApply(LocalDateTime applyDate, Long productId, Long brandId);
-}
--- .history/src/main/java/com/example/demo/domain/model/price/PriceRepository_20240729140448.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain.model.price;
-
-import java.time.LocalDateTime;
-
-public interface PriceRepository {
-    Price getPriceToApply(LocalDateTime applyDate, Long productId, Long brandId);
-}
